771c6ebb0ce3c391ad8f72a312e5d83b3abd3ef6
uuyeong <right00730@gmail.com>
2022-06-13 23:11:16 +0900

    sidemenu 임시구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import SideBar from "./component/UI/SideBar";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello</p>
       </header>
+      <SideBar></SideBar>
     </div>
   );
 }
