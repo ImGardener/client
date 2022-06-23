22b6335c2c2362674e21ce5ecb61036d0f6156e1
uuyeong <right00730@gmail.com>
2022-06-23 16:12:16 +0900

    update css sidemenu small screen

--- src/component/UI/SideBar/SideBar.js
+++ src/component/UI/SideBar/SideBar.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import classes from "./SideBar.module.css";
-import { faSpa, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSpa,
+  faSearch,
+  faBarsStaggered,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import Input from "../Input/Input";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Button from "../Button/Button";
 
 const SideMnu = () => {
   const history = useHistory();
   const [searchWord, setSearchWord] = useState(null);
+  const [menuActive, setMenuActive] = useState(false);
 
   const submitsearchFromHandler = (e) => {
     e.preventDefault();
@@ -17,43 +23,56 @@ const SideMnu = () => {
   const changeSearchInputHandler = (e) => {
     setSearchWord(e.target.value);
   };
+  const showSideBarHandler = () => {
+    setMenuActive((prevState) => !prevState);
+  };
   return (
-    <div className={classes.sidenav}>
-      <section id="search" alt="" className={classes["search"]}>
-        <form
-          method="post"
-          action="#"
-          className={classes["search__form"]}
-          onSubmit={submitsearchFromHandler}
-        >
-          <Input
-            input={{
-              type: "text",
-              name: "search",
-              placeholder: "검색하기",
-            }}
-            onChange={changeSearchInputHandler}
-            inputClassName={classes["search__input"]}
-            icon={faSearch}
-          />
-        </form>
-      </section>
-      <ul className={classes["side-menu"]}>
-        <li className={classes["side-menu__item"]}>
-          <Link to={"/search"} className={classes["item__title"]}>
-            <FontAwesomeIcon icon={faSearch} />
-            검색하기
-          </Link>
-        </li>
+    <>
+      <div className={`${classes.sidenav} ${menuActive && classes["active"]}`}>
+        <section id="search" alt="" className={classes["search"]}>
+          <form
+            method="post"
+            action="#"
+            className={classes["search__form"]}
+            onSubmit={submitsearchFromHandler}
+          >
+            <Input
+              input={{
+                type: "text",
+                name: "search",
+                placeholder: "검색하기",
+              }}
+              onChange={changeSearchInputHandler}
+              inputClassName={classes["search__input"]}
+              icon={faSearch}
+            />
+          </form>
+        </section>
+        <ul className={classes["side-menu"]}>
+          <li className={classes["side-menu__item"]}>
+            <Link to={"/search"} className={classes["item__title"]}>
+              <FontAwesomeIcon icon={faSearch} />
+              검색하기
+            </Link>
+          </li>
 
-        <li className={classes["side-menu__item"]}>
-          <Link to={"/myPlants"} className={classes["item__title"]}>
-            <FontAwesomeIcon icon={faSpa} />
-            My Plants
-          </Link>
-        </li>
-      </ul>
-    </div>
+          <li className={classes["side-menu__item"]}>
+            <Link to={"/myPlants"} className={classes["item__title"]}>
+              <FontAwesomeIcon icon={faSpa} />
+              My Plants
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Button
+        className={`${classes["toggle-btn"]} ${
+          menuActive && classes["active"]
+        }`}
+        onClick={showSideBarHandler}
+      >
+        <FontAwesomeIcon icon={faBarsStaggered} />
+      </Button>
+    </>
   );
 };
 
