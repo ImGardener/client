f5b4706d21d2434779bab4127766c37ae71952be
uuyeong <proyeongs@gmail.com>
2022-06-30 17:20:09 +0900

    오늘의꽃 조회 이미지로드 수정, 최적화

--- src/component/FlowerOfDay/FlowerOfDay.js
+++ src/component/FlowerOfDay/FlowerOfDay.js
@@ -1,33 +1,35 @@
+import React, { useCallback } from "react";
 import classes from "./FlowerOfDay.module.css";
 import { useEffect } from "react";
 import { getTodayFlower } from "../../utils/flower-apis";
 import { useState } from "react";
 import LoadingSpinner from "../UI/Spinner/LoadingSpinner";
 import useHttp from "../../hoc/use-https";
+import img from "../../assets/sample_plants.jpg";
 
-const FlowerOfDay = (props) => {
+const FlowerOfDay = () => {
   const { requestHandler, error, status, data } = useHttp();
   const [flowerInfo, setFlowerInfo] = useState({
     name: "거베라",
     img: "",
   });
+  const getFlowerOfDay = useCallback(async () => {
+    await requestHandler(getTodayFlower);
+  }, [requestHandler]);
 
   useEffect(() => {
     getFlowerOfDay();
-  }, []);
+  }, [getFlowerOfDay]);
   useEffect(() => {
     if (status === "SUCCESS" && data) {
       setFlowerInfo(data);
     } else if (error) {
       setFlowerInfo({
         name: "거베라",
-        img: "http://www.nongsaro.go.kr/portal/imgView.do?ep=a5gb/CMEYLclIUPoWw9/DTnomi9S/YDfHD2ofIDhNOqPWPXCk6r5f9/5EwSYQ8lvB@14eQKulp3yt2AmBvaAPM3fCBe83GYJtr@G9Xgc/x4!",
+        img: img,
       });
     }
   }, [status, error, data]);
-  const getFlowerOfDay = async () => {
-    await requestHandler(getTodayFlower);
-  };
 
   if (status === "PENDING") return <LoadingSpinner />;
   return (
@@ -50,4 +52,4 @@ const FlowerOfDay = (props) => {
     </div>
   );
 };
-export default FlowerOfDay;
+export default React.memo(FlowerOfDay);
